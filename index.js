1f4efab3511ec9eaaeab4a9cc92e32a914983bf2
Phuc Le <phuclevinh2000@gmail.com>
2021-12-15 19:04:15 +0200

    upto lesson 11

---
+++ index.js
@@ -0,0 +1,25 @@
+import { createServer } from 'http';
+
+const hostname = 'localhost';
+const port = 5000;
+
+const server = createServer((req, res) => {
+  const { url } = req;
+  console.log(url);
+
+  if (url === '/translation') {
+    const translations = {1: 'one', 2: 'two'}
+
+    res.setHeader('Content-Type', "application/json");
+
+    res.write(JSON.stringify(translations))
+
+    res.end()
+  }
+  
+  res.end('Welcome to node');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server runnning at ${hostname}: ${port}`);
+});
